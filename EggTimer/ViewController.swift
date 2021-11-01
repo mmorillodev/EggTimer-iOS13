fe1d353e281473349bc507c20e0c1dc431ca380b
lamatheus.g <m.goncalves@trustetc.com>
2021-11-01 20:41:30 -0300

    finish timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let hardnessDict = [
-        "Soft": 5,
-        "Medium": 7,
-        "Hard": 12,
+    static let secondsInAMinute = 60
+    static let hardnessDict = [
+        "Soft": 1 * secondsInAMinute,
+        "Medium": 7 * secondsInAMinute,
+        "Hard": 12 * secondsInAMinute,
     ]
-
+    
+    var timer: Timer?
+    var secondsRemaining = 0
+    
+    @IBOutlet weak var label: UILabel!
+    
     @IBAction func hardenessSelected(_ sender: UIButton) {
-        print(hardnessDict[sender.titleLabel!.text!] ?? "Not found")
+        if(timer != nil) {
+            timer!.invalidate()
+        }
+
+        secondsRemaining = ViewController.hardnessDict[sender.currentTitle!] ?? 0
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(startTimer), userInfo: nil, repeats: true)
     }
     
+    @objc func startTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining)")
+            secondsRemaining -= 1
+        } else {
+            label.text = "Done!"
+            timer!.invalidate()
+        }
+    }
 }
